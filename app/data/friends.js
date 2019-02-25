ddd34c3395ec8cab4abd611b202947d433f0263b
Brzeczyszczykiewicz <Samproschansky@gmail.com>
2019-02-25 08:51:01 -0500

    Fixed photo paths.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,32 +1,34 @@
+const path  = require('path');
+
 var friends = [
 	{
 		name: "Mat",
-		photo: "app/img/mat.jpg",
+		photo: path.join(__dirname, "app/img/mat.jpg"),
 		scores: [2, 1, 2, 1, 1, 2, 2, 1, 1, 1]
 	},
 	{
-		name: "Travis",
-		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRinEZvZsKJ5yuTsD1ug6Hq5OEwYkv-sQjCNsOTwaNrx0_B-VDy6g",
+		name: "Chad",
+		photo: path.join(__dirname, "app/img/chad.png"),
 		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
 	},
 	{
 		name: "Art",
-		photo: "app/img/art.jpg",
+		photo: path.join(__dirname,"app/img/art.jpg"),
 		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
 	},
 	{
 		name: "Jazz",
-		photo: "app/img/jazz.jpg",
+		photo: path.join(__dirname,"app/img/jazz.jpg"),
 		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
 	},
 	{
 		name: "Derrick",
-		photo: "app/img/derrick.jpg",
+		photo: path.join(__dirname, "app/img/derrick.jpg"),
 		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
 	},
 	{
 		name:"Sue",
-		photo: "app/img/sue.jpg",
+		photo: path.join(__dirname,"app/img/sue.jpg"),
 		scores: [2,3,1,1,4,5,5,3,2,1]
 	}
 ];
